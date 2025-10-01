d2809dff2211d19214e6fefd89f7677274b43e85
Arseni-B <arseniyberdnik@gmail.com>
2025-10-01 22:37:40 +0300

    minor refactoring of routers

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { ModeToggle } from "@/components/mode-toggle"
+import { Navigate } from 'react-router-dom';
 import Auth from "@/pages/auth"
 import News from "@/pages/news"
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/auth/*" element={<Auth />} />
           <Route path="/news/*" element={<News />} />
+          <Route path="*" element={<Navigate to="/news" replace />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
